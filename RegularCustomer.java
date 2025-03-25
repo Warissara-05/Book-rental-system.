3d1f3a80033aba578bde4874c90d5af3b3078880
Warissara-05 <xarmdiw@gmail.com>
2025-03-25 16:06:24 +0700

    Delete RegularCustomer.java

--- RegularCustomer.java
+++
@@ -1,8 +0,0 @@
-package customer;
-
-public class RegularCustomer extends Customer {
-
-    public RegularCustomer(String name, String email, String phoneNumber) {
-        super(name, email, phoneNumber);
-    }
-}
